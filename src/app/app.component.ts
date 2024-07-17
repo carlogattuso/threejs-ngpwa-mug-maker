4a88ab371fb88611b54efa90dfe66ff76c06047b
cgattuso <carlo.gattuso@soprasteria.com>
2024-07-17 11:29:53 +0200

    Three Integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import {Component} from '@angular/core';
+import {ApplicationRef, Component, inject, PLATFORM_ID, signal, WritableSignal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {Button} from "primeng/button";
 import {InputTextModule} from "primeng/inputtext";
 import {SplitButtonModule} from "primeng/splitbutton";
 import {ToolbarModule} from "primeng/toolbar";
-import {FileSelectEvent, FileUploadModule} from "primeng/fileupload";
+import {FileUploadEvent, FileUploadModule} from "primeng/fileupload";
 import {ColorPickerModule} from "primeng/colorpicker";
 import {FormsModule} from "@angular/forms";
-import {NgForOf} from "@angular/common";
+import {isPlatformBrowser, NgForOf} from "@angular/common";
 import {DialogModule} from "primeng/dialog";
 import {ColorDialogComponent} from "./core/ui/color-dialog/color-dialog.component";
+import {first} from "rxjs";
+import {MugComponent} from "./core/ui/mug/mug.component";
 
 @Component({
   selector: 'app-root',
@@ -25,48 +27,56 @@ import {ColorDialogComponent} from "./core/ui/color-dialog/color-dialog.componen
     FormsModule,
     NgForOf,
     DialogModule,
-    ColorDialogComponent
+    ColorDialogComponent,
+    MugComponent
   ],
   styleUrl: './app.component.scss',
   template: `
-    <p-toolbar styleClass="shadow-1">
-      <div class="p-toolbar-group-start">
+    <p-toolbar styleClass="absolute border-noround w-full shadow-1">
+      <div class="p-toolbar-group-start flex flex-wrap gap-2">
         <p-fileUpload
           mode="basic"
           name="demo[]"
           chooseIcon="pi pi-upload"
-          accept="image/*"
-          maxFileSize="1000000"
-          (onSelect)="onSelectDesign($event)"
-          [chooseLabel]="selectedFilename"
+          url="https://www.primefaces.org/cdn/api/upload.php"
+          accept="image/*" maxFileSize="1000000"
+          (onUpload)="onFileUploadAuto($event)"
           [auto]="true"
-          class="mr-2"
+          [chooseLabel]="selectedFile ? selectedFile.name : 'Sube tu diseño'"
         />
-        <p-button [outlined]="true" icon="pi pi-download" label="Descarga la plantilla"/>
-      </div>
-      <div class="p-toolbar-group-center">
-        <p-button (onClick)="isColorSelectionVisible = true" severity="secondary" label="Personalizar colores"
+        <p-button (onClick)="isColorSelectionVisible = true" label="Colorea tu taza"
                   icon="pi pi-palette"/>
+        <p-button [outlined]="true" icon="pi pi-download" label="Descarga la plantilla"/>
+        <p-button [outlined]="true" severity="danger" label="Borrar diseño" icon="pi pi-trash"/>
         <app-color-dialog (hideColorSelectionDialogEvent)="isColorSelectionVisible = false"
                           [isColorSelectionVisible]="_isColorSelectionVisible"></app-color-dialog>
       </div>
-      <div class="p-toolbar-group-end">
-        <p-button [outlined]="true" severity="danger" label="Borrar diseño" icon="pi pi-trash"/>
-      </div>
     </p-toolbar>
+    <app-mug></app-mug>
   `
 })
 export class AppComponent {
-  title = 'mug-maker';
+  static isBrowser: WritableSignal<boolean> = signal(false);
+  static isStable: WritableSignal<boolean> = signal(false);
+  title: string = 'mug-maker';
   _isColorSelectionVisible: boolean = false;
-  selectedFilename: string = "Sube tu diseño";
+  selectedFile: File | undefined;
+  private platformId: object = inject(PLATFORM_ID);
+  private applicationRef: ApplicationRef = inject(ApplicationRef);
 
-  onSelectDesign(event: FileSelectEvent) {
-    this.selectedFilename = event.files[0].name;
-    console.log('design uploaded');
+  constructor() {
+    AppComponent.isBrowser.set(isPlatformBrowser(this.platformId));
+    this.applicationRef.isStable.pipe(
+      first((stable: boolean) => stable)
+    ).subscribe((stable: boolean) => AppComponent.isStable.set(stable));
   }
 
   set isColorSelectionVisible(value: boolean) {
     this._isColorSelectionVisible = value;
   }
+
+  onFileUploadAuto($event: FileUploadEvent): void {
+    this.selectedFile = $event.files[0];
+  }
+
 }
